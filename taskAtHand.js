0750caaba4bfbd57386fdc0315eb3c5776ceb513
andres.cuervo <andres.cuervo@tecnilogica.com>
2016-09-07 16:19:29 +0200

    change data with click

--- taskAtHand.js
+++ taskAtHand.js
@@ -51,6 +51,33 @@ function TaskAtHandApp()
 			$task.insertAfter($task.next());
 		});
 
+		$("span.task-name", $task).click(function() {
+			onEditTaskName($(this));
+		});
+
+		$("input.task-name", $task).change(function(){
+			onChangeTaskName($(this));
+		}).blur(function() {
+			$(this).hide().siblings("span.task-name").show();
+		});
+	}
+
+	function onEditTaskName($span)
+	{
+		$span.hide()
+			.siblings("input.task-name")
+			.val($span.text())
+			.show()
+			.focus();
+	}
+
+	function onChangeTaskName($input) {
+		$input.hide();
+		var $span = $input.siblings("span.task-name");
+		if ($input.val()) {
+			$span.text($input.val());
+		}
+		$span.show();
 	}
 }
 
